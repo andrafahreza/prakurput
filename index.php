2edf7563186a174f3c6d9540fee30a31115098d4
Muhammad Andra <andrafahreza818@gmail.com>
2023-10-30 22:31:05 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+        <!-- Meta Tags -->
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0">
+        <title>Hehe</title>
+
+        <!-- Favicon and touch Icons -->
+        <link href="images/favicon.png" rel="shortcut icon" type="image/png">
+        <link href="images/apple-touch-icon.html" rel="apple-touch-icon">
+        <link href="images/apple-touch-icon-72x72.html" rel="apple-touch-icon" sizes="72x72">
+        <link href="images/apple-touch-icon-114x114.html" rel="apple-touch-icon" sizes="114x114">
+        <link href="images/apple-touch-icon-144x144.html" rel="apple-touch-icon" sizes="144x144">
+
+        <!-- Lead Style -->
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+    </head>
+
+	
+	<!-- Preloader -->
+	<div class="oculoader">
+		<div class="loader"></div>
+		<div class="top-side"></div>
+		<div class="bottom-side"></div>
+	</div>
+	<!--PreLoader End-->
+	
+    <body class="body_box dark_mode">
+		<audio  autoplay id="myAudio" preload="auto">
+			<source src="images/lagu.mp3" type="audio/mpeg">
+		</audio>
+
+		<!-- <iframe src="images/lagu.mp3" allow="autoplay"> -->
+		
+		<div class="wrapper">
+	        
+			<div class="inner_body">
+				<div class="slide_1">
+					<div class="swiper-container">
+						<div class="swiper-wrapper">
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/1.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/2.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/3.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/4.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/5.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/6.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/7.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/8.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/9.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/10.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						  	<div class="swiper-slide">
+								<div class="slider_item" style="background-image:url('images/11.jpg')">
+									<div class="slider-inner">
+										<div class="slider-details">
+											<h1 class="slide_heading"><span>27 Bulan Mei</span></h1>
+											<div class="button_group groupPlay">
+												<a href="#" class="button effect_1 fade_black_button" onclick="play()"><span>Play Music</span></a>
+											</div>
+											<div class="button_group groupPause d-none">
+												<a href="#" class="button effect_1 fade_black_button" onclick="pause()"><span>Pause Music</span></a>
+											</div>
+										</div>
+									</div>
+								</div>
+						  	</div>
+						</div>
+					</div>
+					<div class="swiper-controller">
+						<!-- Add Pagination -->
+						<div class="swiper-pagination"></div>
+
+						<!-- If we need navigation buttons -->
+						<div class="swiper-navigation">
+							<div class="swiper-button-prev"></div>
+							<div class="swiper-button-next"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="page_title"><span>Pratiwi Kurnia Putri</span></div>
+			<footer class="footer">
+				<div class="footer_inner">
+					<div class="copyright">
+						<p> &copy; <?php echo date('Y'); ?></p>
+					</div>
+				</div>
+			</footer>
+		</div>
+	
+		
+        <!-- All JavaScript Files
+        ================================================== -->
+		<script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+		<script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/imagesloaded.pkgd.min.js"></script>
+        <script src="js/jquery.fancybox.min.js"></script>
+        <script src="js/swiper.min.js"></script>
+        <script src="js/typed.min.js"></script>
+        <script src="js/menu.js"></script>
+        <script src="js/custom.js"></script>
+	</body>
+
+	<script>
+		function play() {
+			var x = document.getElementById("myAudio");
+			x.play();
+			$('.groupPlay').addClass('d-none');
+			$('.groupPause').removeClass('d-none');
+		}
+
+		function pause() {
+			var x = document.getElementById("myAudio");
+			x.pause();
+			$('.groupPlay').removeClass('d-none');
+			$('.groupPause').addClass('d-none');
+		}
+	</script>
+</html>
